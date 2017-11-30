36228ccf08ac991ad9b26f7645074c28fcf1ef6b
Charles Ellis <hamled@hamled.name>
2017-11-29 20:35:01 -0800

    Fetch wizards from HP API and list their names

--- src/app.js
+++ src/app.js
@@ -1,33 +1,55 @@
-import 'foundation-sites/dist/css/foundation.css';
-import './style.css';
-
 import $ from 'jquery';
 import _ from 'underscore';
 
-const rawBookData = [
-  {
-    title: 'Practical Object-Oriented Design in Ruby',
-    author: 'Sandy Metz',
-    publication_year: 2012
-  }, {
-    title: 'Parable of the Sower',
-    author: 'Octavia Butler',
-    publication_year: 1993
-  }, {
-    title: 'A Wizard of Earthsea',
-    author: 'Ursula K. Le Guin',
-    publication_year: 1969
-  }
-];
+import 'foundation-sites/dist/css/foundation.css';
+import './style.css';
+
+import WizardSchool from './collections/wizard_school';
+import Wizard from './models/wizard';
+
 
 // Starts undefined - we'll set this in $(document).ready
 // once we know the template is available
-let bookTemplate;
+let wizardTemplate;
 
-const render = function render(bookList) {
-
+const render = function render(wizardList) {
+  wizardList.forEach((wizard) => {
+    $('#wizards').append(`<li>${wizard.get('name')}</li>`);
+  });
 };
 
 $(document).ready(() => {
-  bookTemplate = _.template($('#book-template').html());
+  wizardTemplate = _.template($('#book-template').html());
+  //
+  // const example = {
+  //   success: true,
+  //   "not success": false,
+  // };
+  //
+  // example.success == true;
+  // example['success'] == true;
+  // example['not success'] == false;
+
+  const school = new WizardSchool();
+  school.fetch({
+    // success: function success() {
+    //   // this is the success callback
+    // },
+    //
+    // failure() {
+    //   // failure callback
+    // },
+  });
+
+  school.on('update', function() {
+    render(school);
+  });
+
+  school.add({
+    name: "Charles",
+    house: "Hufflepuff"
+  });
+
+
+  Wizard.someClassFunction();
 });
